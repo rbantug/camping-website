0c4919005bfa5ea9ad900d98c065c6b0ec928c7c
rbantug <ronbantug22@gmail.com>
2026-01-28 20:55:20 +0800

    build: added tailwindcss + forms, iconify, primevue

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,10 @@
+import './assets/css/main.css'
+import 'primeicons/primeicons.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +13,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    unstyled: true
+})
 
 app.mount('#app')
