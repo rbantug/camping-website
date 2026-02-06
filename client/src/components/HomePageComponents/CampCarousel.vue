b83938b316ed34810a3cba7ea48415b65adc8685
rbantug <ronbantug22@gmail.com>
2026-02-06 16:15:10 +0800

    feat: updated the styling

--- client/src/components/HomePageComponents/CampCarousel.vue
+++ client/src/components/HomePageComponents/CampCarousel.vue
@@ -97,52 +97,50 @@ const testCamps = ref([
           :loop="true"
         >
           <swiper-slide v-for="camp in testCamps" :key="camp.name">
-            <div class="">
-              <div class="max-w-90 mx-auto rounded-2xl group md:max-w-85 lg:max-w-100">
-                <div class="h-65 rounded-t-2xl overflow-hidden">
-                  <img
-                    :src="'/src/assets/images/' + camp.image"
-                    :alt="camp.name"
-                    class="object-cover object-center h-65 w-100 rounded-t-2xl group-hover:scale-120 duration-300"
-                    :class="{ grayscale: camp.status === 'full' }"
-                  />
-                </div>
-                <div
-                  class="h-90 max-w-100 bg-white rounded-b-2xl -translate-y-2 group-hover:-translate-y-4 duration-300 md:shadow"
-                >
-                  <div class="w-[90%] mx-auto">
-                    <!-- Camp name and description -->
-                    <h3
-                      class="text-2xl font-semibold pt-8 group-hover:text-accent-primary duration-200"
-                      :class="{ 'text-xl': camp.name.length > 20 }"
-                    >
-                      {{ camp.name }}
-                    </h3>
-                    <p class="pt-2 text-neutral-600">{{ camp.shortDescription }}</p>
-                    <!-- Amenities Badge -->
-                    <div class="flex gap-x-2 pt-4">
-                      <div v-for="n in 2" :key="n">
-                        <base-badge :name="camp.amenities[n]" class="" />
-                      </div>
+            <div class="max-w-90 mx-auto rounded-2xl group md:max-w-85 lg:max-w-100">
+              <div class="h-65 rounded-t-2xl overflow-hidden">
+                <img
+                  :src="'/src/assets/images/' + camp.image"
+                  :alt="camp.name"
+                  class="object-cover object-center h-65 w-100 rounded-t-2xl group-hover:scale-120 duration-300"
+                  :class="{ grayscale: camp.status === 'full' }"
+                />
+              </div>
+              <div
+                class="h-90 max-w-100 bg-white rounded-b-2xl -translate-y-2 group-hover:-translate-y-4 duration-300 md:shadow"
+              >
+                <div class="w-[90%] mx-auto flex flex-col">
+                  <!-- Camp name and description -->
+                  <h3
+                    class="text-2xl font-semibold pt-8 group-hover:text-accent-primary duration-200"
+                    :class="{ 'text-xl': camp.name.length > 20, 'pb-1': camp.name.length > 20 }"
+                  >
+                    {{ camp.name }}
+                  </h3>
+                  <p class="pt-2 text-neutral-600">{{ camp.shortDescription }}</p>
+                  <!-- Amenities Badge -->
+                  <div class="flex gap-x-2 pt-4">
+                    <div v-for="n in 2" :key="n">
+                      <base-badge :name="camp.amenities[n]" class="" />
                     </div>
-                    <div v-if="camp.amenities?.length > 2" class="pl-2 pt-1">
-                      Plus {{ camp.amenities.length - 2 }} more
+                  </div>
+                  <div v-if="camp.amenities?.length > 2" class="pl-2 pt-1">
+                    Plus {{ camp.amenities.length - 2 }} more
+                  </div>
+                  <hr class="text-neutral-400 font-bold mt-8" />
+                  <!-- Price -->
+                  <div class="mt-6 flex flex-col md:flex-row md:justify-between md:items-center">
+                    <div>
+                      <span class="text-xl font-semibold tracking-wider">${{ camp.price }}</span>
+                      <span class="text-sm text-neutral-500"> /night</span>
                     </div>
-                    <hr class="text-neutral-400 font-bold mt-8" />
-                    <!-- Price -->
-                    <div class="mt-6 flex flex-col md:flex-row md:justify-between md:items-center">
-                      <div>
-                        <span class="text-xl font-semibold tracking-wider">${{ camp.price }}</span>
-                        <span class="text-sm text-neutral-500"> /night</span>
-                      </div>
-                      <PrimaryButton
-                        size="default"
-                        :label="camp.status === 'available' ? 'Book now' : 'Full'"
-                        :group-animate="true"
-                        class="mt-4 md:mt-0"
-                        :disabled="camp.status === 'available' ? false : true"
-                      />
-                    </div>
+                    <PrimaryButton
+                      size="default"
+                      :label="camp.status === 'available' ? 'Book now' : 'Full'"
+                      :group-animate="true"
+                      class="mt-4 md:mt-0"
+                      :disabled="camp.status === 'available' ? false : true"
+                    />
                   </div>
                 </div>
               </div>
