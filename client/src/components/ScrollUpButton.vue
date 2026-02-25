77e872cf2756cf6c63f664229feeb52718c1a011
rbantug <ronbantug22@gmail.com>
2026-02-25 16:10:55 +0800

    feat: added scrollUpButton component

---
+++ client/src/components/ScrollUpButton.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
+
+const showScrollToTopBtn = ref(false)
+const notAtTopOfWebpage = ref(false)
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
+
+function handleScroll() {
+  showScrollToTopBtn.value = window.scrollY > 800
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+  notAtTopOfWebpage.value = window.screenY > 800
+})
+onUnmounted(() => window.removeEventListener('scroll', handleScroll))
+</script>
+
+<template>
+  <div
+    class="fixed w-15 h-15 bg-accent-secondary rounded-full bottom-10 right-5 flex items-center justify-center cursor-pointer hover:-translate-y-2 hover:bg-accent-primary duration-300 z-10" :class="{ 'opacity-0': !showScrollToTopBtn && !notAtTopOfWebpage, 'opacity-100': showScrollToTopBtn || notAtTopOfWebpage }"
+    @click="scrollToTop"
+  >
+    <div class="text-white scale-200">
+      <!-- mdi:arrow-up -->
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M13 20h-2V8l-5.5 5.5l-1.42-1.42L12 4.16l7.92 7.92l-1.42 1.42L13 8z"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
