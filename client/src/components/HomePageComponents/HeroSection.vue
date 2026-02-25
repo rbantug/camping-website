04b4b350193e3919dfad7f0a0594ecc59dee666c
rbantug <ronbantug22@gmail.com>
2026-02-25 23:48:47 +0800

    fix: changed the styling of the secondary button

--- client/src/components/HomePageComponents/HeroSection.vue
+++ client/src/components/HomePageComponents/HeroSection.vue
@@ -34,7 +34,7 @@ const outputBtnSize = computed(() => {
         :size="outputBtnSize"
         label="Explore camps"
       />
-      <SecondaryButton :size="outputBtnSize" label="About us" dark-mode-no-ring  />
+      <SecondaryButton :size="outputBtnSize" label="About us" dark-mode-no-ring no-ring  />
     </div>
   </div>
 </template>
