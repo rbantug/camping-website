23d4d063c1f65fd2d349417c1039a029fa3daabf
rbantug <ronbantug22@gmail.com>
2026-02-25 16:09:10 +0800

    feat: added tailwindcss dark mode

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
-  import 'iconify-icon'
-  import Button from 'primevue/button';
-
+  import TransitionScroll from './components/util/TransitionScroll.vue';
   import TheHeader from './components/TheHeader.vue';
   import TheFooter from './components/TheFooter.vue';
+  import ScrollUpButton from './components/ScrollUpButton.vue';
+  import ToggleDarkModeButton from './components/ToggleDarkModeButton.vue';
 </script>
 
 <template>
-  <main class="font-DM-Sans">
-    <TheHeader />
+  <main class="font-DM-Sans transition-colors duration-300">
+    <TransitionScroll :z-index="1" :relative="true">
+      <TheHeader />
+    </TransitionScroll>
     <RouterView />
     <TheFooter />
+
+    <ScrollUpButton />
+    <ToggleDarkModeButton />
   </main>
 
 </template>
