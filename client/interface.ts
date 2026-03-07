f16d8c9162f04f3f20174a3e5e90e368b3612619
rbantug <ronbantug22@gmail.com>
2026-03-07 14:32:08 +0800

    feat: all typescript interfaces are stored in interface.ts

---
+++ client/interface.ts
@@ -0,0 +1,10 @@
+export interface Camps {
+    name: string;
+    shortDescription: string;
+    fullDescription: string;
+    price: string;
+    image: string;
+    blurryImg: string;
+    status: string;
+    amenities: string[];
+}
