e5362508fc1de51f04a50b22a05fa5a4c20cbd1a
rbantug <ronbantug22@gmail.com>
2026-03-07 14:36:56 +0800

    feat: added usehead for preloading image used for homepage hero section

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-  import { onBeforeMount } from 'vue';
-  import { useHead } from '@unhead/vue'
+import { onBeforeMount } from 'vue'
+import { useHead } from '@unhead/vue'
 
-  import TransitionScroll from './components/util/TransitionScroll.vue';
-  import TheHeader from './components/TheHeader.vue';
-  import TheFooter from './components/TheFooter.vue';
-  import ScrollUpButton from './components/ScrollUpButton.vue';
-  import ToggleDarkModeButton from './components/ToggleDarkModeButton.vue';
+import TransitionScroll from './components/util/TransitionScroll.vue'
+import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
+import ScrollUpButton from './components/ScrollUpButton.vue'
+import ToggleDarkModeButton from './components/ToggleDarkModeButton.vue'
 
-  useHead({
+useHead({
   link: [
     {
       rel: 'preload',
       as: 'image',
-      href: 'https://dl.dropboxusercontent.com/scl/fi/a1q1m6i4529oba7ls4kh6/camping-stars-2.avif?rlkey=14kkjyab0xaii38ns6pz5f0jy'
-    }
-  ]
+      href: 'https://dl.dropboxusercontent.com/scl/fi/a1q1m6i4529oba7ls4kh6/camping-stars-2.avif?rlkey=14kkjyab0xaii38ns6pz5f0jy',
+    },
+  ],
 })
 
-  onBeforeMount(() => {
-    const saved = localStorage.getItem('theme')
+onBeforeMount(() => {
+  const saved = localStorage.getItem('theme')
 
-      if (saved === 'dark') {
-      document.documentElement.classList.add('dark')
-    }
-  })
+  if (saved === 'dark') {
+    document.documentElement.classList.add('dark')
+  }
+})
 </script>
 
 <template>
@@ -33,17 +33,16 @@
       <TheHeader />
     </TransitionScroll>
     <!-- <RouterView /> -->
-     <router-view v-slot="{ Component }">
+    <router-view v-slot="{ Component }">
       <KeepAlive>
         <component :is="Component" />
       </KeepAlive>
-     </router-view>
+    </router-view>
     <TheFooter />
 
     <ScrollUpButton />
     <ToggleDarkModeButton />
   </main>
-
 </template>
 
 <style scoped></style>
