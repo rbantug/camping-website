561a4020eb4674625b499bad9d1b2a5e96776766
rbantug <ronbantug22@gmail.com>
2026-03-18 23:28:14 +0800

    feat: added dynamic route loading and scrollBehavior always goes to the top of the webpage

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomePage from '@/components/router/HomePage.vue'
-import AboutPage from '@/components/router/AboutPage.vue'
-import AllCamps from '@/components/router/AllCamps.vue'
-import CampDetails from '@/components/router/CampDetails.vue'
-import ContactPage from '@/components/router/ContactPage.vue'
-import AuthPage from '@/components/router/AuthPage.vue'
-import ErrorMessage from '@/components/router/ErrorMessage.vue'
+const HomePage = () => import('@/components/router/HomePage.vue')
+const AboutPage = () => import('@/components/router/AboutPage.vue')
+const AllCamps = () => import('@/components/router/AllCamps.vue')
+const CampDetails = () => import('@/components/router/CampDetails.vue')
+const ContactPage = () => import('@/components/router/ContactPage.vue')
+const AuthPage = () => import('@/components/router/AuthPage.vue')
+const ErrorMessage = () => import('@/components/router/ErrorMessage.vue')
+const ConvertURL = () => import('@/components/router/ConvertURL.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,11 +15,15 @@ const router = createRouter({
     { name: 'home', path: '/home', component: HomePage, alias: '/' },
     { name: 'about', path: '/about', component: AboutPage },
     { name: 'allCamps', path: '/camps', component: AllCamps },
-    { name: 'campDetails', path: '/camps/:id', component: CampDetails },
+    { name: 'campDetails', path: '/camps/:slug', component: CampDetails },
     { name: 'contact', path: '/contact', component: ContactPage },
     { name: 'auth', path: '/auth', component: AuthPage },
+    { name: 'convertUrl', path: '/convert', component: ConvertURL },
     { path: '/:anything(.*)', component: ErrorMessage },
   ],
+  scrollBehavior (to, from, savedPosition) {
+    return { top: 0, left: 0 }
+  }
 })
 
 export default router
