711bdcc4809ef3b240c47926b9a28635c6213283
rbantug <ronbantug22@gmail.com>
2026-03-19 22:46:10 +0800

    feat: - The entire CampCard is now clickable and will redirect users to CampDetails - Those camps with status "full" are not clickable and styling on hover will not work

--- client/src/components/BaseComponents/CampCard.vue
+++ client/src/components/BaseComponents/CampCard.vue
@@ -1,35 +1,58 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+
 import type { Camps } from '../../../interface'
 
-import BaseBadge from './Buttons/BaseBadge.vue';
-import PrimaryButton from './Buttons/PrimaryButton.vue';
-import LazyLoadImage from '../util/LazyLoadImage.vue';
+import BaseBadge from './Buttons/BaseBadge.vue'
+import PrimaryButton from './Buttons/PrimaryButton.vue'
+import LazyLoadImage from '../util/LazyLoadImage.vue'
 
-type Prop = Omit<Camps, "imageHires">
+type Prop = Omit<Camps, 'imageHires'>
+
+const router = useRouter()
 
 const props = defineProps<Prop>()
+
+function goToCampDetails() {
+  if (props.status === 'full') return
+  
+  router.push({
+    name: 'campDetails',
+    params: {
+      slug: props.slug,
+    },
+  })
+}
 </script>
 
 <template>
-  <div>
+  <div :class="{ 'cursor-pointer': props.status === 'available' }" @click="goToCampDetails">
     <div class="max-w-90 mx-auto rounded-2xl group md:max-w-85 lg:max-w-90">
       <div class="h-65 rounded-t-2xl overflow-hidden">
         <LazyLoadImage
           :alt-name="props.name"
           :img-path="props.image"
           :blurry-img-path="props.image"
-          class="object-cover object-center h-65 w-100 rounded-t-2xl group-hover:scale-120 duration-300"
-          :class="{ grayscale: props.status === 'full' }"
+          class="object-cover object-center h-65 w-100 rounded-t-2xl duration-300"
+          :class="[
+            { 'grayscale': props.status === 'full' },
+            { 'group-hover:scale-105': props.status === 'available' },
+          ]"
         />
       </div>
       <div
-        class="h-90 max-w-100 rounded-b-2xl -translate-y-2 group-hover:-translate-y-4 duration-300 md:shadow bg-white dark:bg-neutral-800"
+        class="h-90 max-w-100 rounded-b-2xl -translate-y-2 duration-300 md:shadow bg-white dark:bg-neutral-800"
+        :class="{ 'group-hover:-translate-y-4': props.status === 'available' }"
       >
         <div class="w-[90%] mx-auto flex flex-col">
           <!-- Camp name and description -->
           <h3
-            class="text-2xl font-semibold pt-8 group-hover:text-accent-primary duration-200 dark:text-white dark:group-hover:text-accent-secondary transition-colors"
-            :class="{ 'text-xl': props.name.length > 20, 'pb-1': props.name.length > 20 }"
+            class="text-2xl font-semibold pt-8 duration-200 dark:text-white transition-colors"
+            :class="[
+              { 'text-xl': props.name.length > 20, 'pb-1': props.name.length > 20 },
+              { 'group-hover:text-accent-primary': props.status === 'available' },
+              { 'dark:group-hover:text-accent-secondary': props.status === 'available' },
+            ]"
           >
             {{ props.name }}
           </h3>
@@ -39,7 +62,7 @@ const props = defineProps<Prop>()
           <!-- Amenities Badge -->
           <div class="flex gap-x-2 pt-4">
             <div v-for="n in 2" :key="n">
-              <base-badge :name="props.amenities[n]" class="" />
+              <base-badge :name="props.amenities[n]" />
             </div>
           </div>
           <div
