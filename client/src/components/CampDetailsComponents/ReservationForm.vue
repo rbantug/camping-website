95bd1f93d393469c4da69df93018225eeb4d8353
rbantug <ronbantug22@gmail.com>
2026-03-26 18:06:40 +0800

    feat: added TransitionScroll to the form

--- client/src/components/CampDetailsComponents/ReservationForm.vue
+++ client/src/components/CampDetailsComponents/ReservationForm.vue
@@ -2,57 +2,67 @@
 import { ref } from 'vue'
 
 import PrimaryButton from '../BaseComponents/Buttons/PrimaryButton.vue'
+import TransitionScroll from '../util/TransitionScroll.vue'
 
 const props = defineProps({
   price: {
-    type: Number,
-    required: true
-  }
+    type: String,
+    required: true,
+  },
 })
 
 const nights = ref(1)
 const date = ref()
-
 </script>
 
 <template>
   <div class="flex justify-center pt-10 lg:-mt-200">
-    <div class="bg-neutral-100 rounded-3xl border-2 border-neutral-500 h-135 w-full max-w-90 dark:bg-neutral-900 transition-color duration-300 dark:border-neutral-400 md:max-w-190 lg:max-w-100">
-    <div
-      class="w-[85%] py-10 mx-auto"
-    >
-      <div>
-        <!-- Heading -->
-        <h1 class="text-2xl font-bold text-neutral-900 dark:text-neutral-100">Reserve Camp</h1>
-        <p class="mt-2 text-neutral-700 dark:text-neutral-400 md:text-lg">
-          Vitae pellentesque sem placerat in id cursus mi. Cursus mi pretium tellus duis convallis
-          tempus leo.
-        </p>
-      </div>
-      <!-- Price -->
-      <div class="mt-5">
-        <div class="text-neutral-700 dark:text-neutral-400 text-lg">From</div>
-        <span class="font-bold text-xl text-neutral-900 dark:text-neutral-100">$ {{ props.price }} USD</span>
-        <span class="text-neutral-700 dark:text-neutral-400 text-lg"> /night</span>
-      </div>
-      <hr class="my-8 text-neutral-700 dark:text-neutral-400"/>
-      <!-- Input boxes -->
-      <div class="w-full gap-x-3 grid grid-cols-[60%_40%] md:w-90 lg:gap-0">
-        <div class="flex flex-col items-start gap-y-3 w-45">
-          <span class="font-bold text-neutral-900 dark:text-neutral-100">Start Date</span>
-          <input type="date" class="w-full h-15 rounded-4xl text-neutral-900 border-2 focus:border-accent-primary hover:border-accent-primary dark:focus:border-accent-outline dark:hover:border-accent-outline dark:text-neutral-400 dark:border-neutral-400  dark:bg-neutral-900 dark:scheme-dark">
+    <TransitionScroll delay="1s">
+      <div
+        class="bg-neutral-100 rounded-3xl border-2 border-neutral-500 h-135 w-full max-w-90 dark:bg-neutral-900 transition-color duration-300 dark:border-neutral-400 md:max-w-190 lg:max-w-100"
+      >
+        <div class="w-[85%] py-10 mx-auto">
+          <div>
+            <!-- Heading -->
+            <h1 class="text-2xl font-bold text-neutral-900 dark:text-neutral-100">Reserve Camp</h1>
+            <p class="mt-2 text-neutral-700 dark:text-neutral-400 md:text-lg">
+              Vitae pellentesque sem placerat in id cursus mi. Cursus mi pretium tellus duis
+              convallis tempus leo.
+            </p>
+          </div>
+          <!-- Price -->
+          <div class="mt-5">
+            <div class="text-neutral-700 dark:text-neutral-400 text-lg">From</div>
+            <span class="font-bold text-xl text-neutral-900 dark:text-neutral-100"
+              >$ {{ props.price }} USD</span
+            >
+            <span class="text-neutral-700 dark:text-neutral-400 text-lg"> /night</span>
+          </div>
+          <hr class="my-8 text-neutral-700 dark:text-neutral-400" />
+          <!-- Input boxes -->
+          <div class="w-full gap-x-3 grid grid-cols-[60%_40%] md:w-90 lg:gap-0">
+            <div class="flex flex-col items-start gap-y-3 w-45">
+              <span class="font-bold text-neutral-900 dark:text-neutral-100">Start Date</span>
+              <input
+                type="date"
+                class="w-full h-15 rounded-4xl text-neutral-900 border-2 focus:border-accent-primary hover:border-accent-primary dark:focus:border-accent-outline dark:hover:border-accent-outline dark:text-neutral-400 dark:border-neutral-400 dark:bg-neutral-900 dark:scheme-dark"
+              />
+            </div>
+            <div class="flex flex-col items-start gap-y-3 w-25">
+              <span class="font-bold text-neutral-900 dark:text-neutral-100">Nights</span>
+              <input
+                type="number"
+                class="rounded-4xl w-full h-15 text-neutral-900 border-2 focus:border-accent-primary hover:border-accent-primary dark:focus:border-accent-outline dark:hover:border-accent-outline dark:text-neutral-400 dark:border-neutral-400 dark:bg-neutral-900 dark:scheme-dark"
+                v-model="nights"
+              />
+            </div>
+          </div>
+          <!-- buttons -->
+          <div class="mt-10">
+            <PrimaryButton label="Add to Cart" size="large" />
+          </div>
         </div>
-        <div class="flex flex-col items-start gap-y-3 w-25">
-          <span class="font-bold text-neutral-900 dark:text-neutral-100">Nights</span>
-          <input type="number" class="rounded-4xl w-full h-15 text-neutral-900  border-2 focus:border-accent-primary hover:border-accent-primary dark:focus:border-accent-outline dark:hover:border-accent-outline dark:text-neutral-400 dark:border-neutral-400  dark:bg-neutral-900 dark:scheme-dark" v-model="nights">
-        </div>
       </div>
-      <!-- buttons -->
-       <div class="mt-10">
-        <PrimaryButton label="Add to Cart" size="large" />
-       </div>
-    </div>
+    </TransitionScroll>
   </div>
-  </div>
-  
 </template>
