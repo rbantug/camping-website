76d35172602d36432b33b415118e376be8f35852
rbantug <ronbantug22@gmail.com>
2026-04-11 11:57:52 +0800

    feat: ContactPage route is done

--- client/src/components/router/ContactPage.vue
+++ client/src/components/router/ContactPage.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ContactDetails from '../ContactPageComponents/ContactDetails.vue';
+import FrequentlyAskedQuestions from '../ContactPageComponents/FrequentlyAskedQuestions.vue';
+import TheForm from '../ContactPageComponents/TheForm.vue';
+import TopTitle from '../ContactPageComponents/TopTitle.vue';
+
+</script>
 
 <template>
-    <div></div>
+    <!-- width percentages are mobile: 90%, tablet: 80%, desktop: 70%  -->
+    <div class="bg-neutral-100 dark:bg-neutral-900 transition-colors duration-300">
+        <div class="h-fit">
+            <TopTitle />
+            <div class="relative">
+                <TheForm />
+                <ContactDetails />
+            </div>
+            <FrequentlyAskedQuestions />
+        </div>
+    </div>
 </template>
