ee00ff24a00ee2060e2ac78acf5ff9e2ce3670ff
rbantug <ronbantug22@gmail.com>
2026-04-13 16:32:54 +0800

    feat: pages nav menu now has a smooth animation

--- client/src/components/HeaderComponents/NavMenu.vue
+++ client/src/components/HeaderComponents/NavMenu.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref, type PropType } from 'vue'
 
+import { useMainStore } from '@/stores/mainStore'
+
 import PrimaryButton from '../BaseComponents/Buttons/PrimaryButton.vue'
 import type { NavItems } from '../TheHeader.vue'
 
@@ -41,21 +43,57 @@ function closeNavMenu() {
     >
       <div class="flex flex-col gap-y-4">
         <div v-for="{ link, route } in navItems" :key="link">
-          <router-link :to="route" class="text-black dark:text-white transition-color duration-300" @click="closeNavMenu">{{ link }}</router-link>
+          <router-link
+            :to="route"
+            class="text-black dark:text-white transition-color duration-300"
+            @click="closeNavMenu"
+            >{{ link }}</router-link
+          >
         </div>
       </div>
-      <div class="flex cursor-pointer pt-4 pb-2 text-black dark:text-white transition-color duration-300" @click="toggleMoreNavItems">
+      <div
+        class="flex cursor-pointer pt-4 pb-2 text-black dark:text-white transition-color duration-300"
+        @click="toggleMoreNavItems"
+      >
         <span>Pages</span>
         <!-- iconamoon:arrow-down-2 -->
-        <div :class="{ 'rotate-180 duration-200 ease': moreNavItems, 'duration-200 ease': !moreNavItems }">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 10l5 5m0 0l5-5"/></svg>
+        <div
+          :class="{
+            'rotate-180 duration-200 ease': moreNavItems,
+            'duration-200 ease': !moreNavItems,
+          }"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m7 10l5 5m0 0l5-5"
+            />
+          </svg>
         </div>
       </div>
-      <div v-if="moreNavItems" class="flex flex-col gap-2 translate-x-4 pb-4">
-        <div v-for="{ link, route } in additionalNavItems" :key="link">
-          <router-link :to="route" class="text-black dark:text-white transition-color duration-300" @click="closeNavMenu">{{ link }}</router-link>
+      <Transition
+        enter-active-class="transition-all duration-200 ease-out md:duration-300"
+        enter-from-class="opacity-0 max-h-0"
+        enter-to-class="opacity-100 max-h-40"
+        leave-active-class="transition-all duration-150 ease-in md:duration-200"
+        leave-from-class="opacity-100 max-h-40"
+        leave-to-class="opacity-0 max-h-0"
+      >
+        <div v-if="moreNavItems" class="flex flex-col gap-2 translate-x-4 pb-4">
+          <div v-for="{ link, route } in additionalNavItems" :key="link">
+            <router-link
+              :to="route"
+              class="text-black dark:text-white transition-color duration-300"
+              @click="closeNavMenu"
+              >{{ link }}</router-link
+            >
+          </div>
         </div>
-      </div>
+      </Transition>
       <div class="w-full md:w-[20%]">
         <PrimaryButton size="default" label="Book now" />
       </div>
