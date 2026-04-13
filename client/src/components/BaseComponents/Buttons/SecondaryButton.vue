83ca050485d34292b837f2426a080c78f7fbe943
rbantug <ronbantug22@gmail.com>
2026-04-14 00:08:42 +0800

    feat: added route redirection

--- client/src/components/BaseComponents/Buttons/SecondaryButton.vue
+++ client/src/components/BaseComponents/Buttons/SecondaryButton.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { computed } from 'vue'
+import { useRouter } from 'vue-router'
 
 interface Props {
-  size: 'small' | 'default' | 'large';
-  label: string;
-  altColor?: boolean;
-  darkModeNoRing?: boolean;
-  noRing?: boolean;
-  whiteRing?: boolean;
+  size: 'small' | 'default' | 'large'
+  label: string
+  altColor?: boolean
+  darkModeNoRing?: boolean
+  noRing?: boolean
+  whiteRing?: boolean
+  routePath?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -15,17 +17,19 @@ const props = withDefaults(defineProps<Props>(), {
   darkModeNoRing: false,
   noRing: false,
   whiteRing: true,
+  routePath: '/',
 })
 
-const rootStyling = 'bg-white rounded-full w-full cursor-pointer hover:bg-accent-secondary hover:ring-transparent hover:-translate-y-1 duration-200 group dark:bg-neutral-900 transition-color'
+const rootStyling =
+  'bg-white rounded-full w-full cursor-pointer hover:bg-accent-secondary hover:ring-transparent hover:-translate-y-1 duration-200 group dark:bg-neutral-900 transition-color'
 
 const outputRootStyle = computed(() => {
   let newStyle = rootStyling
 
-   if (!props.darkModeNoRing) {
+  if (!props.darkModeNoRing) {
     newStyle = newStyle + ' dark:ring-2 dark:ring-white dark:hover:ring-transparent'
   }
-  
+
   if (!props.noRing) {
     newStyle += ' ring-2'
   }
@@ -35,16 +39,19 @@ const outputRootStyle = computed(() => {
   }
 
   if (props.altColor) {
-    newStyle = newStyle.replace('bg-white', 'bg-transparent').replace('hover:bg-accent-secondary', 'hover:bg-white')
+    newStyle = newStyle
+      .replace('bg-white', 'bg-transparent')
+      .replace('hover:bg-accent-secondary', 'hover:bg-white')
   }
 
   return newStyle
 })
 
-const txtStyle = 'text-neutral-800 group-hover:text-white dark:text-white dark:group-hover:text-neutral-900'
+const txtStyle =
+  'text-neutral-800 group-hover:text-white dark:text-white dark:group-hover:text-neutral-900'
 
 const outputTxtStyle = computed(() => {
-    const newStyle = txtStyle
+  const newStyle = txtStyle
 
   if (props.altColor) {
     return 'text-white group-hover:text-neutral-800'
@@ -52,12 +59,38 @@ const outputTxtStyle = computed(() => {
 
   return newStyle
 })
+
+const router = useRouter()
+
+function gotoRoute() {
+  router.push(props.routePath)
+}
 </script>
 
 <template>
   <div>
-    <button :class="[outputRootStyle, { 'px-3 py-2': props.size === 'small', 'px-5 py-4': props.size === 'default', 'px-7 py-5': props.size === 'large' }]">
-      <span :class="[outputTxtStyle, { 'text-sm': props.size === 'small', 'text-md': props.size === 'default', 'text-lg': props.size === 'large' }]">{{ props.label }}</span>
+    <button
+      :class="[
+        outputRootStyle,
+        {
+          'px-3 py-2': props.size === 'small',
+          'px-5 py-4': props.size === 'default',
+          'px-7 py-5': props.size === 'large',
+        },
+      ]"
+      @click="gotoRoute"
+    >
+      <span
+        :class="[
+          outputTxtStyle,
+          {
+            'text-sm': props.size === 'small',
+            'text-md': props.size === 'default',
+            'text-lg': props.size === 'large',
+          },
+        ]"
+        >{{ props.label }}</span
+      >
     </button>
   </div>
 </template>
