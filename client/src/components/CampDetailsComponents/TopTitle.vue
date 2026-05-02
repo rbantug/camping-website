d9f546e60b17a2cb11a349589e36f5a0429f991b
rbantug <ronbantug22@gmail.com>
2026-05-02 10:58:26 +0800

    styling: adjusted margin for the CampDetails component

--- client/src/components/CampDetailsComponents/TopTitle.vue
+++ client/src/components/CampDetailsComponents/TopTitle.vue
@@ -23,7 +23,7 @@ const props = defineProps<Props>()
     />
     <div class="absolute h-120 top-0 bg-black/20 w-full flex flex-col justify-end md:h-150">
       <TransitionScroll threshold no-animation>
-        <div class="ml-10 lg:w-150">
+        <div class="ml-[7vw] lg:w-150">
           <h1 data-test="header" class="text-neutral-100 text-3xl font-bold tracking-wide mb-3 md:text-5xl lg:text-6xl">{{ props.name }}</h1>
           <p data-test="description" class="text-neutral-100 leading-8 mb-10 md:text-lg">{{ props.shortDescription }}</p>
         </div>
